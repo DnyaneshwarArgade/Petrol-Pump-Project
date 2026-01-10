e2f7e5983facc8b7bbd244df9e6fcd991c0db755
omlotake <om7261524@gmail.com>
2026-01-10 12:57:12 +0530

    Dashboard Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 
-import Layout from './Layout/Layout';
+import Bottom from './Components/Bottom';
+import Navbar from './Components/Navbar';
 
 import Dashboard from './Pages/Dashboard';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import Nozzle from './Pages/Nozzle';
-import Reports from './Pages/Reports';
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import Nozzle from './Pages/Nozzle'
+import Reports from './Pages/Reports'
 import StaffManagement from './Pages/StaffManagement';
 
-function App() {
+
+function App()
+{
   return (
+
     <BrowserRouter>
-      <Routes>
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-        <Route element={<Layout />}>
+      <div>
+        {/* <Navbar /> */}
+        <Routes>
+          <Route path="/Navbar" element={<Navbar />} />
+          <Route path='/Bottom' element={<Bottom />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/nozzle" element={<Nozzle />} />
           <Route path="/reports" element={<Reports />} />
           <Route path="/staffmanagement" element={<StaffManagement />} />
-        </Route>
-
-      </Routes>
+        </Routes>
+        {/* <Bottom /> */}
+      </div>
     </BrowserRouter>
-  );
+  )
 }
-
-export default App;
+export default App; 
