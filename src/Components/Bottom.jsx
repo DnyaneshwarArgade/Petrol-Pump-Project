f5d54ab6cbe3ece0ecc80c6b8e59343d906a0142
unknown <morek0360@gmail.com>
2026-01-11 18:24:10 +0530

    Bottom Navbar

--- src/Components/Bottom.jsx
+++ src/Components/Bottom.jsx
@@ -1,12 +1,31 @@
 import React from 'react';  
+import '../Components/Bottom.css';
 
-function Bottom() 
-{
-    return(
-        <div>
-            <h1>Bottom Page</h1>
-        </div>
-    )
-}
-export default Bottom;  
+
+import { NavLink } from "react-router-dom";
+import { FaHome, FaUsers, FaChartBar } from "react-icons/fa";
+
+const BottomNav = () => {
+  return (
+    <nav className="bottom-nav">
+      <NavLink to="/Dashboard" className="nav-item">
+        <FaHome className="icon" />
+        <span>Dashboard</span>
+      </NavLink>
+
+      <NavLink to="/StaffManagement" className="nav-item">
+        <FaUsers className="icon" />
+        <span>Staff Management</span>
+      </NavLink>
+
+      <NavLink to="/Reports" className="nav-item">
+        <FaChartBar className="icon" />
+        <span>Reports</span>
+      </NavLink>
+    </nav>
+  );
+};
+
+export default BottomNav;
+
 
