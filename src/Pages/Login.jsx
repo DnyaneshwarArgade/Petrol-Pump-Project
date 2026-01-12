788dc5bef1c9f7c3dc28338b16a5c2c1444940da
rutu-04 <rutujavarpe04@gmail.com>
2026-01-12 23:03:14 +0530

    Login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import '../Pages/pages.css';
+import '../Pages/Login.css'; 
 
-function Login() 
-{
-    return(
-        <div>
-            <h1>Login Page</h1>
+const Login = () => {
+  return (
+    <div className="login-page">
+      <div className="login-card">
+        
+         {/* Header  */}
+        <div className="login-header">
         </div>
-    )
-}
-export default Login;  
+
+        {/* Form */}
+        <div className="login-body">
+          <div className="input-group">
+            <span className="input-icon">👤</span>
+            <input type="text" placeholder="Username" />
+          </div>
+
+          <div className="input-group">
+            <span className="input-icon">🔒</span>
+            <input type="password" placeholder="Password" />
+          </div>
+
+          <div className="forgot">
+            <a href="#">Forgot Password?</a>
+          </div>
+
+          <button className="login-btn">Login</button>
+
+          <div className="register">
+            Don’t have an account? <a href="#">Register</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
